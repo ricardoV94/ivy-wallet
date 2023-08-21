68ba3cc6e3916e4901331b5e5411ba58e150fdb5
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2023-08-21 14:07:35 +0200

    Bump version number

--- buildSrc/src/main/java/com/ivy/wallet/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/ivy/wallet/buildsrc/dependencies.kt
@@ -22,8 +22,8 @@ import org.gradle.kotlin.dsl.project
 
 object Project {
     //Version
-    const val versionName = "4.3.13"
-    const val versionCode = 127
+    const val versionName = "4.3.14"
+    const val versionCode = 128
 
     //Compile SDK & Build Tools
     const val compileSdkVersion = 31
