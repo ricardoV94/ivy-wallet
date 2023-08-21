91f7b8e7f315e7a2f7b3d3fa8937ee7b539b9dfe
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2023-08-21 11:18:15 +0200

    Upgrade gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 
 dependencies {
     //https://mvnrepository.com/artifact/com.android.tools.build/gradle?repo=google
-    implementation("com.android.tools.build:gradle:7.1.1")
+    implementation("com.android.tools.build:gradle:7.4.2")
 
     //Must match kotlinVersion from dependencies.kt
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
@@ -27,7 +27,7 @@ dependencies {
     implementation("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
 
     //URL: https://developers.google.com/android/guides/google-services-plugin
-    implementation("com.google.gms:google-services:4.3.10")
+    implementation("com.google.gms:google-services:4.3.15")
 
     implementation("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
 }
